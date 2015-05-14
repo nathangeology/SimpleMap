d3218bcf4a26fdac4ae83c8cc0a013a31a6730c4
Nathan <nathanieljones@Nathaniels-MacBook-Pro-2.local>
2015-05-14 06:59:18 -0700

    5/14/2015 Getting the initial mapping files created and organized. The plan is to create the, files and then build up the basic mapping and data source model-view-controller pattern. The next step after that will be to improve the database IO functionality, then the last step will be to polish the appearance and functionality of the app (and hook it up to some public drilling databases, make it easy to import custom ones, setup notifications of changes, etc)

---
+++ SimpleMap/GS_MK_MapDataSource.swift
@@ -0,0 +1,9 @@
+//
+//  GS_MK_MapDataSource.swift
+//  SimpleMap
+//
+//  Created by Nathaniel Jones on 5/14/15.
+//  Copyright (c) 2015 Nate Geo LLC. All rights reserved.
+//
+
+import Foundation
---
+++ SimpleMap/GS_MK_MapView.swift
@@ -0,0 +1,21 @@
+//
+//  GS_MK_MapView.swift
+//  SimpleMap
+//
+//  Created by Nathaniel Jones on 5/14/15.
+//  Copyright (c) 2015 Nate Geo LLC. All rights reserved.
+//
+
+import UIKit
+
+class GS_MK_MapView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
